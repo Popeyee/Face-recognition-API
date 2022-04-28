611c71600378242a0b751f9c404880ed86d54083
Alii_ <33094586+Popeyee@users.noreply.github.com>
2022-04-28 11:20:05 -0400

    final commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const saltRounds = 10;
+const cors = require('cors');
+const knex = require('knex')
+const register = require ('./controllers/register.js')
+const singin = require ('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'alielalaoui',
+    password : '',
+    database : 'smart-brain11'
+  }
+});
+
+// db.select('*').from('users');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+app.get('/', (req, res) => {res.send(db.users)})
+
+app.post('/signin', (req, res) => { singin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+
+app.listen(3000, () => {
+	console.log('app is working');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

51771604ac013e54793b46cd0f5e7de68b1742e0
Alii_ <33094586+Popeyee@users.noreply.github.com>
2023-01-13 11:24:05 -0500

    clarifai adjustment..

--- controllers/image.js
+++ controllers/image.js
@@ -10,7 +10,6 @@ const handleApiCall = (req, res) => {
     .predict('53e1df302c079b3db8a0a36033ed2d15',req.body.input)
     .then(data => {
         res.json(data)
-        console.log('hiiiii',data)
     })
     .catch(err => res.status(400).json('unable to work with API'))
 
@@ -18,12 +17,10 @@ const handleApiCall = (req, res) => {
 
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
-	console.log(req.body);
 	db('users').where('id', '=', id)
  	.increment('entries', 1)
  	.returning('entries')
  	.then(entries => {
-		 console.log(entries);
  		res.json(entries[0].entries);
  	})
  	.catch(err => res.status(400).json('Unable to get entries'))
